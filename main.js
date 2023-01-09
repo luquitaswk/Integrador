f0c6c2ea1d1d482e52c8a7f3cf08d27622a4ac5d
Lucas Guerrero <lucas.cvwk@gmail.com>
2023-01-09 00:03:14 -0300

    Scroll Up Button
    Se agregó un botón para hacer Scroll UP
    (Hecho con HTML/CSS/JS)

---
+++ main.js
@@ -0,0 +1,33 @@
+// Scroll up
+
+document.getElementById("buttonUp").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
+    }
+}
+
+
+///
+
+buttonUp = document.getElementById("buttonUp");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+
+}
+
+alert("estoy probando");
